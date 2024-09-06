089167d0d16eff14e79b2fe1a9f8952ead453cb6
Dark <175629882+Dark-1738@users.noreply.github.com>
2024-09-06 01:37:59 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 author 'Dark'
-lua54        'yes'
+lua54 'yes'
 
 shared_script '@ox_lib/init.lua'
 
-client_script 'source/client/main.lua'
+client_script 'client.lua'
 
-server_script 'source/server/main.lua'
+server_script 'server.lua'
 
 dependency 'ox_lib'
