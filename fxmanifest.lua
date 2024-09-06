7288d1c1e383b1e07dc0fafc279c1524148e17ab
Dark <175629882+Dark-1738@users.noreply.github.com>
2024-09-06 01:36:24 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Dark'
+lua54        'yes'
+
+shared_script '@ox_lib/init.lua'
+
+client_script 'source/client/main.lua'
+
+server_script 'source/server/main.lua'
+
+dependency 'ox_lib'
